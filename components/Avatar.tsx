5c3f19b3ffe0ded3978327618090bd1045005982
Rahul <rajamohanty12345@gmail.com>
2023-04-13 13:27:56 +0530

    twitter clone updated

---
+++ components/Avatar.tsx
@@ -0,0 +1,47 @@
+import { useCallback } from "react";
+import useUser from "@/hooks/useUser";
+import { useRouter } from "next/router";
+import Image from "next/image";
+
+interface AvatarProps {
+  userId: string;
+  isLarge?: boolean;
+  hasBorder?: boolean;
+}
+
+const Avatar: React.FC<AvatarProps> = ({ userId, isLarge, hasBorder }) => {
+  const { data: fetchedUser } = useUser(userId);
+  const router = useRouter();
+
+  const onClick = useCallback(
+    (event: any) => {
+      event.stopPropagation();
+
+      const url = `/users/${userId}`;
+
+      router.push(url);
+    },
+    [router, userId]
+  );
+  return (
+    <div
+      className={` ${hasBorder ? "border-4 border-black" : ""} ${
+        isLarge ? "h-32 w-32" : "h-12 w-12"
+      } rounded-full hover:opacity-80 transition cursor-pointer relative `}
+    >
+      <Image
+        fill
+        sizes="small"
+        style={{
+          objectFit: "cover",
+          borderRadius: "100%",
+        }}
+        alt="avatar"
+        onClick={onClick}
+        src={fetchedUser?.profileImage || "/images/placeholder.png"}
+      />
+    </div>
+  );
+};
+
+export default Avatar;
---
+++ components/layout/SidebarTweetButton.tsx
@@ -0,0 +1,27 @@
+import { useCallback } from "react";
+import useLoginModal from "@/hooks/useLoginModal";
+import { useRouter } from "next/router";
+import { FaFeather } from "react-icons/fa";
+const SidebarTweetButton = () => {
+  const router = useRouter();
+  const loginModal = useLoginModal();
+
+  const onClick = useCallback(() => {
+    loginModal.onOpen();
+  }, [loginModal]);
+  
+  return (
+    <div onClick={onClick}>
+      <div className="mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center justify-center bg-sky-500 hover:bg-opacity-80 transition cursor-pointer">
+        <FaFeather size={24} color="white" />
+      </div>
+      <div className="mt-6 lg:block hidden px-4 py-2 rounded-full bg-sky-500 hover:bg-opacity-80 transition cursor-pointer">
+        <p className="hidden lg:block text-center font-semibold text-white text-[20px]">
+          Tweet
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SidebarTweetButton;
